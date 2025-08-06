acbf7c445a8e8503e65ac637865be0223da737c4
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-08-06 11:13:26 +0530

    Update Q1.kt

--- .week1/.assign1/Q1.kt
+++ .week1/.assign1/Q1.kt
@@ -1,23 +1,16 @@
 fun main(){
-    val num=5
-    if(num%2==0){
-        println("Even Number")
+    var num = 5
+    var i = 1
+    var count = 0
+    for(i in 1..num){
+        if(num%i==0){
+            count++;
+        }
+    }
+    if(count>2){
+        println("Not Prime")
     }
     else{
-        println("Odd Number")
+        println("Prime number: ${num}")
     }
 }
-/*
-fun isPrime(num: Int): Boolean {
-    if (num <= 1) return false
-    for (i in 2 until num) {
-        if (num % i == 0) return false
-    }
-    return true
-}
-
-fun main() {
-    val number = 29
-    println("$number is prime: ${isPrime(number)}")
-}
-*/
