286bba09baf3cc47db69af589033f88dd52c1b5b
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-08-06 10:00:52 +0530

    Create kt_6_8_25_i.kt

---
+++ kt_6_8_25_i.kt
@@ -0,0 +1,14 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+*/
+
+fun main(){
+    println("Hello, Kotlin for the first time")
+    
+    var name= "DOyel"
+    println(name)
+    
+    val num1= 100
+    println(num1)
+}
