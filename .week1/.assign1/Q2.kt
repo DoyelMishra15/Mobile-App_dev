f3a8572c7156e3ca9d46527a8068e98aa6539980
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-08-06 11:00:38 +0530

    Update Q2.kt

--- .week1/.assign1/Q2.kt
+++ .week1/.assign1/Q2.kt
@@ -1,1 +1,13 @@
+fun factorial(n: Int): Long {
+    var result = 1L
+    for (i in 1..n) {
+        result *= i
+    }
+    return result
+}
+
+fun main() {
+    val num = 5
+    println("Factorial of $num is: ${factorial(num)}")
+}
 
