bb1102bbf54187bb5cfc105c9a163766286ab293
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-08-06 11:01:12 +0530

    Create Q3.kt

---
+++ .week1/.assign1/Q3.kt
@@ -0,0 +1,11 @@
+fun displayPlanets() {
+    val planets = listOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    println("Planets of the Solar System:")
+    for (planet in planets) {
+        println(planet)
+    }
+}
+
+fun main() {
+    displayPlanets()
+}
