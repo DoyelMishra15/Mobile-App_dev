ad66d1f79e84e7257b98c9fc9078c16239fbf27e
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-08-06 10:58:16 +0530

    Update Q1.kt

--- .week1/.assign1/Q1.kt
+++ .week1/.assign1/Q1.kt
@@ -7,3 +7,17 @@ fun main(){
         println("Odd Number")
     }
 }
+/*
+fun isPrime(num: Int): Boolean {
+    if (num <= 1) return false
+    for (i in 2 until num) {
+        if (num % i == 0) return false
+    }
+    return true
+}
+
+fun main() {
+    val number = 29
+    println("$number is prime: ${isPrime(number)}")
+}
+*/
